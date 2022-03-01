f59805f99c445cb2484918c81fe9b9f14b1e0964
sathish-az <sathish.kumar@cloudzenix.com>
2022-03-01 11:01:50 +0000

    nodejs version to 14

--- pipelines_webinar/pipelines_webinar_stack.ts
+++ pipelines_webinar/pipelines_webinar_stack.ts
@@ -14,7 +14,7 @@ export class PipelinesWebinarStack extends Stack {
     const handler = new lambda.Function(this, 'Handler', {
       code: new lambda.AssetCode(path.resolve(__dirname, 'lambda')),
       handler: 'handler.handler',
-      runtime: lambda.Runtime.NODEJS_10_X,
+      runtime: lambda.Runtime.NODEJS_14_X,
     });
 
     const alias = new lambda.Alias(this, 'x', {
